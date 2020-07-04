333bf24830083be56f4fcdefaaba5ad9b0dbd1f2
lavish jain <lavishjain36@gmail.com>
2020-07-04 22:58:30 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,56 @@
+//10+20;
+/*
+if (10+20===30) {
+	//alert("Superb !You have done a great job");
+	console.log("Hello Lavish.");
+}
+*/
+/*
+1 way of creating functions
+function sayHello(){
+      console.log("Hello Kushal");
+
+}
+
+sayHello();
+*/
+
+
+//2nd way of creating functions
+//function Expression
+/*
+var fun= function(){
+	console.log("Kushal");
+}
+
+function fun1(){
+	console.log("Hello Pune");
+	console.log("Hello Udaipur");
+}
+
+fun1();
+*/
+/*
+function fun2(lavish){
+	console.log(lavish);
+}
+
+fun2("Lavish Jain");
+fun2("Kushal Jain");
+fun2("Swapnil");
+*/
+//in javascript ,functions can be assigned to variables
+/*
+function multiply(a,b){
+	return a;
+	return a*b;
+	return b;
+
+}
+multiply(5,10);
+*/
+/*
+var  list=["tiger","cat","bear","bird"];
+console.log(list[1]);
+*/
+var list= ["tiger","cat","bear","bird"];
